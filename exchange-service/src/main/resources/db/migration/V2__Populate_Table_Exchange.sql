08609bf7bfe18af1496d51869c2886d301c2f037
Jonatas Oliveira Silva <JonatasOliveira@ibm.com>
2026-03-03 20:54:19 -0300

    corrigindo migrations.

--- exchange-service/src/main/resources/db/migration/V2__Populate_Table_Exchange.sql
+++ exchange-service/src/main/resources/db/migration/V2__Populate_Table_Exchange.sql
@@ -1,8 +1,7 @@
-INSERT INTO `exchange` (`from_currency`, `to_currency`, `conversion_factor`) VALUES
-                      ('USD', 'BRL', 5.57),
-                      ('USD', 'EUR', 0.87),
-                      ('USD', 'GBP', 0.73),
-                      ('USD', 'ARS', 1189.0),
-                      ('USD', 'CLP', 934.54),
-                      ('USD', 'COP', 4120.0),
-                      ('USD', 'MXN', 19.09);
+INSERT INTO exchange (from_currency, to_currency, conversion_factor) VALUES ('USD', 'BRL', 5.57);
+INSERT INTO exchange (from_currency, to_currency, conversion_factor) VALUES ('USD', 'EUR', 0.87);
+INSERT INTO exchange (from_currency, to_currency, conversion_factor) VALUES ('USD', 'GBP', 0.73);
+INSERT INTO exchange (from_currency, to_currency, conversion_factor) VALUES ('USD', 'ARS', 1189.0);
+INSERT INTO exchange (from_currency, to_currency, conversion_factor) VALUES ('USD', 'CLP', 934.54);
+INSERT INTO exchange (from_currency, to_currency, conversion_factor) VALUES ('USD', 'COP', 4120.0);
+INSERT INTO exchange (from_currency, to_currency, conversion_factor) VALUES ('USD', 'MXN', 19.09);
